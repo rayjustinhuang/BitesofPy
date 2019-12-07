2bd9f7f08f7055d8684432b2ea494e46020e15d0
rayjustinhuang <rayjustinhuang@gmail.com>
2019-12-07 14:45:53 +0800

    Add interim version of bite 200 (enchantable_items.py)

--- enchantable_items.py
+++ enchantable_items.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from urllib.request import urlretrieve
+from collections import defaultdict
 
 from bs4 import BeautifulSoup as Soup
 
@@ -53,6 +54,17 @@ class Item:
             final_string += f' [{num_level}] {name_to_use}\n'
     pass
 
+def extract_items(string):
+    new_string = string.split('/')[-1].split('.')[0]
+    replacements = (('enchanted',''), ('iron',''), ('sm',''), ('fishing_rod', 'fishing rod'))
+    
+    for r in replacements:
+        new_string = new_string.replace(*r)
+    
+    item_list = ['fishing_rod' if item == 'fishing rod' else item for item in new_string.split("_")]
+    item_list = list(filter(None,item_list))
+    
+    return item_list
 
 def generate_enchantments(soup):
     """Generates a dictionary of Enchantment objects
@@ -61,14 +73,23 @@ def generate_enchantments(soup):
     """
     enchantment_dict = defaultdict(Enchantment)
     enchantment_table = soup.find('table', id='minecraft_items')
-    for row in enchantment_table:
+    enchantment_table_data = enchantment_table.find_all('tr')
+    for row in enchantment_table_data[1:]:
+        #print(row)
         name = row.find('a').text
+        #print(name)
         id_name = row.find('em').text
-        max_level = row.find('td').text
+        #print(id_name)
+        max_level = row.find_next('td').find_next('td').text
+        #print(max_level)
         description = row.find('td', class_='hidden-xs').text
-        items = row.find('img')['src'].split('/')[-1].split('.')[0].split("_")
+        #print(description)
+        items = extract_items(row.find('img', class_='img-rounded')['data-src'])
+        #print(items)
         entry = (id_name, name, max_level, description, items)
-        enchantment_dict[id_name].add(*entry)
+        enchantment_dict[id_name] = entry
+        #print('-------------------------------')
+    print(enchantment_dict)
     pass
 
 
@@ -93,7 +114,8 @@ def get_soup(file=HTML_FILE):
         soup = Soup(file, "html.parser")
 
     return soup
-
+soup = get_soup()
+enchantment_data = generate_enchantments(soup)
 
 def main():
     """This function is here to help you test your final code.
@@ -106,8 +128,8 @@ def main():
         print(minecraft_items[item], "\n")
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+    #main()
 
 """
 Armor: 
