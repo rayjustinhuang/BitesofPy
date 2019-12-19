4f3665333a34bbb7819482b2a34a9403c7ad01fb
Justin Huang <rayjustinhuang@gmail.com>
2019-12-19 00:25:11 -0800

    Add zoo.py (bite 138)

--- zoo.py
+++ zoo.py
@@ -1,19 +1,20 @@
 class Animal:
+    animal_list = list()
 
     def __init__(self, name):
         self.animal = name
-        animal_list.append(name.title())
+        Animal.animal_list.append(self.animal)
         pass
 
     def __str__(self):
-        reference = enumerate(animal_list, 10000)[-1]
+        reference = list(enumerate(Animal.animal_list, 10001))[-1]
         return f"{reference[0]}. {reference[1].title()}"
         pass
 
     @classmethod
     def zoo(cls):
         return_string = ''
-        for animal in enumerate(animal_list, 10000):
+        for animal in enumerate(Animal.animal_list, 10001):
             return_string += f"{animal[0]}. {animal[1].title()}\n"
         return return_string[:-1]
         pass
