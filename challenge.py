ada0f7d2baa0d8b1552108bbffc7ed54eff46389
Justin Huang <rayjustinhuang@gmail.com>
2019-12-28 21:57:12 -0800

    Add interim version of bite 24 (challenge.py)

--- challenge.py
+++ challenge.py
@@ -14,20 +14,41 @@ class Challenge(ABC):
     
     @property
     def pretty_title(self):
+        return f'PCC{self.number} - {self.title.title()}'
         pass
 
 
 class BlogChallenge(Challenge):
     
-    def __init__(self, merged_prs):
-        super().__init__(args)
+    def __init__(self, number, title, merged_prs):
+        super().__init__(number, title)
         self.merged_prs = merged_prs
+
+    @abstractmethod
+    def verify(self, check):
+        return check in merged_prs
+        pass        
+
+    @property
+    def pretty_title(self):
+        return f'PCC{self.number} - {self.title}'
+        pass
     pass
 
 
 class BiteChallenge(Challenge):
     
-    def __init__(self, result):
-        super().__init__(args)
+    def __init__(self, number, title, result):
+        super().__init__(number, title)
         self.result = result
+        
+    @abstractmethod
+    def verify(self, check):
+        return check == self.result
+        pass        
+
+    @property
+    def pretty_title(self):
+        return f'Bite {self.number}. {self.title}'
+        pass
     pass
