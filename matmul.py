818ef09acccb006424c84db54f0a6e34b6131033
Justin Huang <rayjustinhuang@gmail.com>
2019-12-30 22:44:18 -0800

    Add interim version of bite 31 (matmul.py)

---
+++ matmul.py
@@ -0,0 +1,16 @@
+class Matrix(object):
+
+    def __init__(self, values):
+        self.values = values
+
+    def __repr__(self):
+        return f'<Matrix values="{self.values}">'
+        
+    def __matmul__(self):
+        pass
+    
+    def __rmatmul__(self):
+        pass
+    
+    def __imatmul__(self):
+        pass
