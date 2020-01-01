e26aaf87c7123a150313654fc1b2704f3cb7fab3
Justin Huang <rayjustinhuang@gmail.com>
2019-12-31 16:57:29 -0800

    Add interim version of bite 31 (matmul.py)

--- matmul.py
+++ matmul.py
@@ -6,11 +6,15 @@ class Matrix(object):
     def __repr__(self):
         return f'<Matrix values="{self.values}">'
         
-    def __matmul__(self):
+    def __matmul__(self, other):
+        dot_product = []
+        for j in range(len(other.values)):
+            for i in range(len(self.values)):
+                dot_product[i][j] = self.values[i] * other.values[j]
         pass
     
-    def __rmatmul__(self):
+    def __rmatmul__(self, other):
         pass
     
-    def __imatmul__(self):
+    def __imatmul__(self, other):
         pass
