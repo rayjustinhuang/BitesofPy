2f0b8208e0e96397afffc4bc78f5cf8ca0f43799
Justin Huang <rayjustinhuang@gmail.com>
2020-01-01 21:48:40 -0800

    Add interim version of bite 31 (matmul.py)

--- matmul.py
+++ matmul.py
@@ -10,20 +10,40 @@ class Matrix(object):
         dot_product = [[0,0],[0,0]]
         for i in range(len(self.values)):
             for j in range(len(other.values)):
-                print(self.values[i][j], other.values[j][i])
-                dot_product[i][j] += self.values[i][j] * other.values[j][i]
+                for k in range(len(self.values)):
+                    dot_product[i][j] += self.values[i][k] * other.values[k][j]
                 
-        return dot_product
+        return Matrix(dot_product)
         pass
     
     def __rmatmul__(self, other):
+        dot_product = [[0,0],[0,0]]
+        for i in range(len(self.values)):
+            for j in range(len(other.values)):
+                for k in range(len(self.values)):
+                    dot_product[i][j] += other.values[i][k] * self.values[k][j]
+                
+        return Matrix(dot_product)
         pass
     
     def __imatmul__(self, other):
+        matrix_copy = self.values.copy()
+        self.values = [[0,0],[0,0]]
+        for i in range(len(self.values)):
+            for j in range(len(other.values)):
+                for k in range(len(self.values)):
+                    #print(matrix_copy[i][k], other.values[k][j])
+                    self.values[i][j] += matrix_copy[i][k] * other.values[k][j]
+                
+        return self
         pass
     
 mat1 = Matrix([[1, 2], [3, 4]])
 mat2 = Matrix([[11, 12], [13, 14]])
 mat3 = mat1 @ mat2
-"[[37, 40], [85, 92]]"
-print(mat3)
+print(mat3.values)
+mat4 = mat2 @ mat1
+print(mat4.values)
+
+mat1 @= mat2
+print(mat1.values)
