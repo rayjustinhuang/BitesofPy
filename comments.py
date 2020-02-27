450415c366a2048ed9d031ef2013189b85ddf589
rayjustinhuang <rayjustinhuang@gmail.com>
2020-02-27 21:35:33 +0700

    Add interim version of bite 179 (comments.py)

--- comments.py
+++ comments.py
@@ -1,6 +1,24 @@
+import re
+
 def strip_comments(code):
     # see Bite description
-    triple_quotes = re.compile(r'"""[.]+"""')
-    single_comments = re.compile(r'#\s')
-    inline comments = re.compile(r'^\s{2}#\s\\n$')
+    triple_quotes = re.compile(r'""".+"""', re.DOTALL)
+    single_comments = re.compile(r'#\s.+')
+    # inline comments = re.compile(r'^\s{2}#\s\\n$')
+    
+    print(re.sub(single_comments, "", code))
     pass
+
+code = '''
+"""this is
+my awesome script
+"""
+# importing modules
+import re
+
+def hello(name):
+    """my function docstring"""
+    return f'hello {name}'  # my inline comment
+'''
+
+strip_comments(code)
