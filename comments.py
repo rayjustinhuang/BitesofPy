c7090737ddcab35cb667710f685a17bbf32ca8c3
Justin Huang <rayjustinhuang@gmail.com>
2020-02-29 19:21:13 +0800

    Add interim version of bite 179 (comments.py)

--- comments.py
+++ comments.py
@@ -2,26 +2,26 @@ import re
 
 def strip_comments(code):
     # see Bite description
-    triple_quotes = re.compile(r'"""[\S\s]*?"""')
-    single_comments = re.compile(r'#\s.+')
+    triple_quotes = re.compile(r'"""[\S\s]*?"""\n')
+    single_comments = re.compile(r'[\s]{2,}#\s.+')
     # inline comments = re.compile(r'^\s{2}#\s\\n$')
     
     no_comments = re.sub(single_comments, "", code)
     no_multiline_strings = re.sub(triple_quotes, "", no_comments)
     
-    print(no_multiline_strings)
+    return no_multiline_strings
     pass
 
 code = '''
-"""this is
-my awesome script
-"""
-# importing modules
-import re
-
-def hello(name):
-    """my function docstring"""
-    return f'hello {name}'  # my inline comment
+def hello_world():
+    # A simple comment preceding a simple print statement
+    print("Hello World")
 '''
 
-strip_comments(code)
+single_comment_after_strip = '''
+def hello_world():
+    print("Hello World")
+'''
+
+#print(strip_comments(code))
+#assert strip_comments(code) == single_comment_after_strip
