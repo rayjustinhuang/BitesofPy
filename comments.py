e9c9b397761429c90321d8a0e4c5589df73bf707
rayjustinhuang <rayjustinhuang@gmail.com>
2020-03-02 23:35:50 +0700

    Add interim version of bite 179 (comments.py)

--- comments.py
+++ comments.py
@@ -3,11 +3,12 @@ import re
 def strip_comments(code):
     # see Bite description
     triple_quotes = re.compile(r'"""[\S\s]*?"""\n')
-    single_comments = re.compile(r"[\s]{1,4}#\s.+\n")
-    # inline comments = re.compile(r'^\s{2}#\s\\n$')
+    single_comments = re.compile(r"[\s]{4}#\s.+\n")
+    inline_comments = re.compile(r'^\s{2}#\s[\S\s]+')
     
     no_comments = re.sub(single_comments, "", code)
-    no_multiline_strings = re.sub(triple_quotes, "", no_comments)
+    no_inlines = re.sub(inline_comments, "", no_comments)
+    no_multiline_strings = re.sub(triple_quotes, "", no_inlines)
     
     return no_multiline_strings
     pass
