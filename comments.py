df6538344e69b99691a89ed3fbaedf97b0ee2cd5
rayjustinhuang <rayjustinhuang@gmail.com>
2020-03-04 22:30:08 +0700

    Add interim version of bite 179 (comments.py)

--- comments.py
+++ comments.py
@@ -2,11 +2,13 @@ import re
 
 def strip_comments(code):
     # see Bite description
+    class_docstrings = re.compile(r'[\s]{8}"""[\S\s]*?"""\n')
     triple_quotes = re.compile(r'[\s]{4}"""[\S\s]*?"""\n')
     single_comments = re.compile(r"[\s]{4}#\s.+\n")
     inline_comments = re.compile(r'^\s{2}#\s[\S\s]+')
     
-    no_comments = re.sub(single_comments, "", code)
+    no_docstrings = re.sub(class_docstrings, "", code)
+    no_comments = re.sub(single_comments, "", no_docstrings)
     no_inlines = re.sub(inline_comments, "", no_comments)
     no_multiline_strings = re.sub(triple_quotes, "", no_inlines)
     
@@ -14,14 +16,19 @@ def strip_comments(code):
     pass
 
 code = '''
-def say_hello(name):
-    """A simple function that says hello... Richie style"""
-    print(f"Hello {name}, is it me you're looking for?")
+class SimpleClass:
+    """Class docstrings go here."""
+
+    def say_hello(self, name: str):
+        """Class method docstrings go here."""
+        print(f'Hello {name}')
 '''
-single_docstring_after_strip = '''
-def say_hello(name):
-    print(f"Hello {name}, is it me you're looking for?")
+class_with_method_after_strip = '''
+class SimpleClass:
+
+    def say_hello(self, name: str):
+        print(f'Hello {name}')
 '''
 
 #print(strip_comments(code))
-#assert strip_comments(code) == single_docstring_after_strip
+#assert strip_comments(code) == class_with_method_after_strip
