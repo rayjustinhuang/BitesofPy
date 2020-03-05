2a3976fe7dbfc4ad625348cd6e0472c4dafa7833
rayjustinhuang <rayjustinhuang@gmail.com>
2020-03-05 23:50:04 +0700

    Add interim version of bite 179 (comments.py)

--- comments.py
+++ comments.py
@@ -2,12 +2,14 @@ import re
 
 def strip_comments(code):
     # see Bite description
+    three_indent_docstrings = re.compile(r'[\s]{12}"""[\S\s]*?"""\n')
     class_docstrings = re.compile(r'[\s]{8}"""[\S\s]*?"""\n')
     triple_quotes = re.compile(r'[\s]{4}"""[\S\s]*?"""\n')
     single_comments = re.compile(r"[\s]{4}#\s.+\n")
     inline_comments = re.compile(r'^\s{2}#\s[\S\s]+')
     
-    no_docstrings = re.sub(class_docstrings, "", code)
+    no_three_indents = re.sub(three_indent_docstrings, "", code)
+    no_docstrings = re.sub(class_docstrings, "", no_three_indents)
     no_comments = re.sub(single_comments, "", no_docstrings)
     no_inlines = re.sub(inline_comments, "", no_comments)
     no_multiline_strings = re.sub(triple_quotes, "", no_inlines)
@@ -22,13 +24,21 @@ class SimpleClass:
     def say_hello(self, name: str):
         """Class method docstrings go here."""
         print(f'Hello {name}')
+
+        def func_in_method(self):
+            """Docstring with 3 indents and multiline
+               should also be stripped
+            """
+            pass
 '''
-class_with_method_after_strip = '''
+class_three_indents_after_strip = '''
 class SimpleClass:
 
     def say_hello(self, name: str):
         print(f'Hello {name}')
-'''
 
-#print(strip_comments(code))
-#assert strip_comments(code) == class_with_method_after_strip
+        def func_in_method(self):
+            pass
+'''
+print(strip_comments(code))
+assert strip_comments(code) == class_three_indents_after_strip
