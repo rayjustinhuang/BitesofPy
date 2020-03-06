176e14745bbf7cb084637696bd12e4ad6c20f8eb
Justin Huang <rayjustinhuang@gmail.com>
2020-03-06 23:06:06 +0800

    Add comments.py (bite 179)

--- comments.py
+++ comments.py
@@ -5,40 +5,18 @@ def strip_comments(code):
     three_indent_docstrings = re.compile(r'[\s]{12}"""[\S\s]*?"""\n')
     class_docstrings = re.compile(r'[\s]{8}"""[\S\s]*?"""\n')
     triple_quotes = re.compile(r'[\s]{4}"""[\S\s]*?"""\n')
+    multiline_quotes = re.compile(r'"""[\S\s]*?"""\n')
     single_comments = re.compile(r"[\s]{4}#\s.+\n")
-    inline_comments = re.compile(r'^\s{2}#\s[\S\s]+')
+    inline_comments = re.compile(r'\s{2}#\s[\S\s]+\n')
+    basic_comments = re.compile(r'#\s[\S\s]*?\n')
     
     no_three_indents = re.sub(three_indent_docstrings, "", code)
     no_docstrings = re.sub(class_docstrings, "", no_three_indents)
     no_comments = re.sub(single_comments, "", no_docstrings)
     no_inlines = re.sub(inline_comments, "", no_comments)
-    no_multiline_strings = re.sub(triple_quotes, "", no_inlines)
+    no_triple_quotes = re.sub(triple_quotes, "", no_inlines)
+    no_basic_comments = re.sub(basic_comments, "", no_triple_quotes)
+    no_multiline_strings = re.sub(multiline_quotes, "", no_basic_comments)
     
     return no_multiline_strings
     pass
-
-code = '''
-class SimpleClass:
-    """Class docstrings go here."""
-
-    def say_hello(self, name: str):
-        """Class method docstrings go here."""
-        print(f'Hello {name}')
-
-        def func_in_method(self):
-            """Docstring with 3 indents and multiline
-               should also be stripped
-            """
-            pass
-'''
-class_three_indents_after_strip = '''
-class SimpleClass:
-
-    def say_hello(self, name: str):
-        print(f'Hello {name}')
-
-        def func_in_method(self):
-            pass
-'''
-print(strip_comments(code))
-assert strip_comments(code) == class_three_indents_after_strip
