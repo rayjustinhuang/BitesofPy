9c57d878de46226764a6a26ad11d01130eb128dc
Justin Huang <rayjustinhuang@gmail.com>
2020-03-15 11:33:42 +0800

    Add interim version of bite 224 (split.py)

--- split.py
+++ split.py
@@ -3,7 +3,7 @@ import re
 def get_sentences(text):
     """Return a list of sentences as extracted from the text passed in.
        A sentence starts with [A-Z] and ends with [.?!]"""
-    sentence = re.compile(r'^[A-Z][\S\s]+[.?!]$')
+    sentence = re.compile(r'[A-Z][^.]+[.?!]')
     
     sentence_list = re.findall(sentence, text)
     
