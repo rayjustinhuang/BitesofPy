1d1c4d8a6bb1ec7a91ff15e97b4c80bcf6d2a36e
Justin Huang <rayjustinhuang@gmail.com>
2020-03-18 22:39:32 +0800

    Add strip.py (bite 224)

--- split.py
+++ split.py
@@ -3,22 +3,17 @@ import re
 def get_sentences(text):
     """Return a list of sentences as extracted from the text passed in.
        A sentence starts with [A-Z] and ends with [.?!]"""
-    sentence = re.compile(r'[A-Z][^.]+[.?!][^A-Z)]*')
+    sentence = re.compile(r'(\s*[!?.])\s([A-Z])')
     
-    sentence_list = re.findall(sentence, text)
+    lines = re.split(sentence, text)
+    
+    lines.insert(0, "")
+    
+    sentence_list = ["".join(lines[n:n+3]) for n in range(0,len(lines),3)]
     
     final_list = []
     for sentence in sentence_list:
-        final_list.append(re.sub('\n', ' ', sentence))
+        final_list.append(re.sub('\n', ' ', sentence).strip())
     
     return final_list
     pass
-
-text = """
-We are looking forward attending the next Pycon in the U.S.A.
-in 2020. Hope you do so too. There is no better Python networking
-event than Pycon. Meet awesome people and get inspired. Btw this
-dot (.) should not end this sentence, the next one should. Have fun!
-"""
-
-print(get_sentences(text))
