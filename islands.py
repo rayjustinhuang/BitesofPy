1e2a3b29a5cd0bda1eab909fffe9a5a4877b8588
Justin Huang <rayjustinhuang@gmail.com>
2020-03-29 22:36:20 +0800

    Add interim version of bite 263 (islands.py)

---
+++ islands.py
@@ -0,0 +1,29 @@
+def count_islands(grid):
+    """
+    Input: 2D matrix, each item is [x, y] -> row, col.
+    Output: number of islands, or 0 if found none.
+    Notes: island is denoted by 1, ocean by 0 islands is counted by continously
+        connected vertically or horizontically  by '1's.
+    It's also preferred to check/mark the visited islands:
+    - eg. using the helper function - mark_islands().
+    """
+    x = len(grid[0])
+    y = len(grid)
+    
+    islands = 0
+    for i in range(x):
+        for j in range(y):
+            if grid[j][i] == 1:
+                pass
+    # islands = 0         # var. for the counts
+    # .....some operations.....
+    # mark_islands(r, c, grid)
+    # return islands
+
+
+def mark_islands(i, j, grid):
+    """
+    Input: the row, column and grid
+    Output: None. Just mark the visisted islands as in-place operation.
+    """
+    # grid[i][j] = '#'      # one way to mark visited ones - suggestion.
