43c8ee9be3e74d6dfaf96a478119f1871fcf038f
Justin Huang <rayjustinhuang@gmail.com>
2020-04-03 22:12:55 +0800

    Add interim version of bite 263 (islands.py)

--- islands.py
+++ islands.py
@@ -33,26 +33,44 @@ def mark_islands(i, j, grid):
     grid[i][j] = 'X'
     
     def check_adjacents(i,j):
-        try: 
-            if grid[i-1][j] == 1:
-                grid[i-1][j] = 'X'
-        except: pass
-        try: 
-            if grid[i+1][j] == 1:
-                grid[i+1][j] = 'X'
-        except: pass
-        try: 
-            if grid[i][j-1] == 1:
-                grid[i][j-1] = 'X'
-        except: pass
-        try: 
-            if grid[i][j+1] == 1:
-                grid[i][j+1] = 'X'
-        except: pass
+        n = [True, True, True, True]
+        cells_to_check = [(i,j)]
+        while any(n):
+            for pair in cells_to_check:
+                i = pair[0]
+                j = pair[1]
+                try: 
+                    if grid[i-1][j] == 1:
+                        grid[i-1][j] = 'X'
+                        n[0] = False
+                    else:
+                        cells_to_check.append((i-1,j))
+                except: pass
+                try: 
+                    if grid[i+1][j] == 1:
+                        grid[i+1][j] = 'X'
+                        n[1] = False
+                    else:
+                        cells_to_check.append((i+1,j))
+                except: pass
+                try: 
+                    if grid[i][j-1] == 1:
+                        grid[i][j-1] = 'X'
+                        n[2] = False
+                    else:
+                        cells_to_check,append((i,j-1))
+                except: pass
+                try: 
+                    if grid[i][j+1] == 1:
+                        grid[i][j+1] = 'X'
+                        n[3] = False
+                    else:
+                        cells_to_check.append((i,j+1))
+                except: pass
     
-    check_adjacents(i,j)
-    check_adjacents(i-1,j)
-    check_adjacents(i+1,j)
-    check_adjacents(i,j-1)
-    check_adjacents(i,j+1)
+    #check_adjacents(i,j)
+    #check_adjacents(i-1,j)
+    #check_adjacents(i+1,j)
+    #check_adjacents(i,j-1)
+    #check_adjacents(i,j+1)
     # grid[i][j] = '#'      # one way to mark visited ones - suggestion.
