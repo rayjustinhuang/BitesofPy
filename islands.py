aad5fe524ff1baec9122191cc1752a680591019d
Justin Huang <rayjustinhuang@gmail.com>
2020-04-07 00:44:27 +0800

    Add interim version of bite 263 (islands.py)

--- islands.py
+++ islands.py
@@ -39,37 +39,42 @@ def mark_islands(i, j, grid):
             for pair in cells_to_check:
                 x = pair[0]
                 y = pair[1]
+                
+                left = abs(x-1)
+                right = abs(x+1)
+                up = abs(y-1)
+                down = abs(y+1)
                 try: 
-                    if grid[x-1][y] == 1:
-                        grid[x-1][y] = 'X'
-                        cells_to_check.append((x-1,y))
+                    if grid[left][y] == 1:
+                        grid[left][y] = 'X'
+                        cells_to_check.append((left,y))
                     else:
                         n[0] = False
                 except: n[0] = False
                 try: 
-                    if grid[x+1][y] == 1:
-                        grid[x+1][y] = 'X'
-                        cells_to_check.append((x+1,y))
+                    if grid[right][y] == 1:
+                        grid[right][y] = 'X'
+                        cells_to_check.append((right,y))
                     else:
                         n[1] = False
                 except: n[1] = False
                 try: 
-                    if grid[x][y-1] == 1:
-                        grid[x][y-1] = 'X'
-                        cells_to_check,append((x,y-1))
+                    if grid[x][up] == 1:
+                        grid[x][up] = 'X'
+                        cells_to_check,append((x,up))
                     else:
                         n[2] = False
                 except: n[2] = False
                 try: 
-                    if grid[x][y+1] == 1:
-                        grid[x][y+1] = 'X'
-                        cells_to_check.append((x,y+1))
+                    if grid[x][down] == 1:
+                        grid[x][down] = 'X'
+                        cells_to_check.append((x,down))
                     else:
                         n[3] = False
                 except: n[3] = False
                 #for i in range(len(grid)):
                 #    print(grid[i])
-                #print()
+                #print(n)
     
     check_adjacents(i, j)
     
@@ -86,9 +91,9 @@ def mark_islands(i, j, grid):
 #           [1, 0, 0, 1, 1, 0],
 #           [1, 1, 1, 1, 0, 0]]
 
-squares = [[1, 1, 0, 1],
-           [1, 1, 0, 1],
-           [0, 0, 1, 1],
-           [1, 1, 1, 0]]
+#squares = [[1, 1, 0, 1],
+#           [1, 1, 0, 1],
+#           [0, 0, 1, 1],
+#           [1, 1, 1, 0]]
            
-print(count_islands(squares))
+#print(count_islands(squares))
