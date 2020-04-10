d7e0d877100e150e4e3741954690915c398aea73
Justin Huang <rayjustinhuang@gmail.com>
2020-04-10 21:59:03 +0800

    Add funds.py (bite 265)

--- funds.py
+++ funds.py
@@ -9,17 +9,17 @@ def max_fund(village):
     running_total = 0
     max_total = 0
     
-    start = 0
-    end = 0
+    max_start = 0
+    max_end = 0
     
-    for i in range(len(village)):
-        for j in range(i, len(village)-i):
+    for i in range(len(village)+1):
+        for j in range(i, len(village)+1):
             running_total = sum(village[i:j])
             if running_total > max_total:
                 max_total = running_total
-                start = i
-                end = j
+                max_start = i + 1
+                max_end = j
                 
                 
-    return (max_total, i, j)
+    return (max_total, max_start, max_end)
     pass
