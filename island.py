d812a6215671b0cbd129d0dae5f0180dfdbbecc9
Justin Huang <rayjustinhuang@gmail.com>
2020-04-12 20:47:47 +0800

    Add interim version of bite 267 (island.py)

--- island.py
+++ island.py
@@ -15,13 +15,24 @@ def get_others(map_, r, c):
     rows = len(map_)
     columns = len(map_[0])
     
-    starting_cell = map[r][c]
+    starting_cell = map_[r][c]
     
-    check_up = lambda grid, x, y: grid[x][y-1]
-    check_left = lambda grid, x, y: grid[x-1][y]
-    check_right = lambda grid, x, y: grid[x+1][y]
-    check_down = lambda grid, x, y: grid[x][y+1]
+    left, up, down, right = max(c-1,0), max(r-1,0), max(r+1,0), max(c+1,0)
     
+    check_left = lambda grid, x, y: grid[x][y-1]
+    check_up = lambda grid, x, y: grid[x-1][y]
+    check_down = lambda grid, x, y: grid[x+1][y]
+    check_right = lambda grid, x, y: grid[x][y+1]
+    
+    print(map_)
+    print(starting_cell)
+    print(check_left(map_, r, c))
+    
+    if starting_cell == 1:
+        try:
+            if check_up(map_, r, c) != 1:
+                nums += 1
+        except: nums += 1
     
 
     return nums
@@ -37,3 +48,10 @@ def island_size(map_):
     # your code here
 
     return perimeter
+    
+rectangle = [[0, 1, 1, 0],
+             [0, 1, 1, 0],
+             [0, 1, 1, 0],
+             [0, 1, 1, 0]]
+
+get_others(rectangle, 0, 0)
