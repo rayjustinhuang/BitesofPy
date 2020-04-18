a525fda171328b4987b28d742c278956a2590870
Justin Huang <rayjustinhuang@gmail.com>
2020-04-18 12:14:57 +0800

    Add interim version of bite 40 (binary_search.py)

--- binary_search.py
+++ binary_search.py
@@ -2,19 +2,18 @@ def binary_search(sequence, target):
     
     length = len(sequence)
     
-    labeled_sequence = zip(range(length), sequence)
-    
-    print(list(labeled_sequence))
+    sequence_copy = sequence.copy()
     
     #while True:
-    halfway = int(len(sequence)/2)
-    print(halfway)
+    while len(sequence) != 1:
+        halfway = int(len(sequence)/2)
+        
+        if target <= sequence[halfway]:
+            sequence = sequence[:halfway]
+        else:
+            sequence = sequence[halfway:]
     
-    if target <= sequence[halfway]:
-        sequence = sequence[:halfway]
+    answer = sequence[0]
     
+    return sequence_copy.index(answer)
     pass
-
-PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61]
-
-binary_search(PRIMES, 2)
