10a5779e76e4db751716cd14d8f0b15c77fa8dc5
Justin Huang <rayjustinhuang@gmail.com>
2020-04-25 20:37:27 +0800

    Add interim version of ops.py (bite 268)

---
+++ ops.py
@@ -0,0 +1,29 @@
+def num_ops(n):
+    """
+    Input: an integer number, the target number
+    Output: the minimum number of operations required to reach to n from 1.
+
+    Two operations rules:
+    1.  multiply by 2
+    2.  int. divide by 3
+
+    The base number is 1. Meaning the operation will always start with 1
+    These rules can be run in any order, and can be run independently.
+
+    [Hint] the data structure is the key to solve it efficiently.
+    """
+    # you code
+    ops_num = 0
+    target = 1
+    
+    while target != n:
+        if target < n:
+            target *= 2
+            ops_num += 1
+        else:
+            target //= 3
+            ops_num += 1
+
+    return ops_num
+    
+print(num_ops(10))
