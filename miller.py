3976b6907ff8afbb2c140f1f344ee870cc2be032
Justin Huang <rayjustinhuang@gmail.com>
2020-05-15 23:37:53 +0800

    Add miller.py (bite 51)

--- miller.py
+++ miller.py
@@ -9,15 +9,24 @@ def py2_earth_hours_left(start_date=BITE_CREATED_DT):
     """Return how many hours, rounded to 2 decimals, Python 2 has
        left on Planet Earth (calculated from start_date)"""
     
-    delta = PY2_DEATH_DT - BITE_CREATED_DT
+    delta = PY2_DEATH_DT - start_date
     
     return round(delta.days *24 + delta.seconds / 3600,2)
     
     pass
 
-print(py2_earth_hours_left())
 
 def py2_miller_min_left(start_date=BITE_CREATED_DT):
     """Return how many minutes, rounded to 2 decimals, Python 2 has
        left on Planet Miller (calculated from start_date)"""
+    
+    delta = PY2_DEATH_DT - start_date
+    
+    earth_hours = round(delta.days *24 + delta.seconds / 3600,2)
+    
+    conversion = 365*24*7
+    
+    miller_minutes = round(earth_hours/conversion * 60,2)
+    
+    return miller_minutes
     pass
