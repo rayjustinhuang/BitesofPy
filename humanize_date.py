75da0b69d79dcc5b12acb872db762c3fe90af100
Justin Huang <rayjustinhuang@gmail.com>
2020-05-22 23:40:37 +0800

    Add interim version of bite 92 (humanize_date.py)

---
+++ humanize_date.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+from datetime import datetime
+
+TimeOffset = namedtuple('TimeOffset', 'offset date_str divider')
+
+NOW = datetime.now()
+MINUTE, HOUR, DAY = 60, 60*60, 24*60*60
+TIME_OFFSETS = (
+    TimeOffset(10, 'just now', None),
+    TimeOffset(MINUTE, '{} seconds ago', None),
+    TimeOffset(2*MINUTE, 'a minute ago', None),
+    TimeOffset(HOUR, '{} minutes ago', MINUTE),
+    TimeOffset(2*HOUR, 'an hour ago', None),
+    TimeOffset(DAY, '{} hours ago', HOUR),
+    TimeOffset(2*DAY, 'yesterday', None),
+)
+
+
+def pretty_date(date):
+    """Receives a datetime object and converts/returns a readable string
+       using TIME_OFFSETS"""
+    if type(date) != datetime:
+        raise ValueError
+    if date > NOW:
+        raise ValueError
+    
+    diff = (NOW - date).total_seconds()
+    
+    if diff <= DAY:
+        for timeoffset in TIME_OFFSETS:
+            if timeoffset.offset > diff:
+                return timeoffset.date_str
+    pass
