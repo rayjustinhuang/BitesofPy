456e9b6cb94b1257d8afabf60aa648d017159e23
Justin Huang <rayjustinhuang@gmail.com>
2020-05-26 23:07:06 +0800

    Add names.py (bite 180)

---
+++ names.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    
+    for line in data.splitlines()[1:]:
+        countries[line[-2:]] += [line[:-3].split(',')[1] + " " + line[:-3].split(',')[0]]
+        
+    return countries
+    # you code
