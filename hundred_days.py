3c0f67521fc347ad55c842235a7acd1584b4f87a
Justin Huang <rayjustinhuang@gmail.com>
2020-06-06 19:41:21 +0800

    Add interim version of bite 147 (hundred_days.py)

--- hundred_days.py
+++ hundred_days.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-import dateutil.rrule
+from dateutil.rrule import rrule, DAILY, MO, TU, WE, TH, FR
 
 TODAY = date(year=2018, month=11, day=29)
 
@@ -9,9 +9,5 @@ def get_hundred_weekdays(start_date=TODAY):
     """Return a list of hundred date objects starting from
        start_date up till 100 weekdays later, so +100 days
        skipping Saturdays and Sundays"""
-    #print(TODAY + dateutil.relativedelta.relativedelta(days=100))
-    
-    print(list(dateutil.rrule.rrule(dateutil.rrule.DAILY, count=100, dtstart=TODAY)))
+    return list(rrule(DAILY, count=100, byweekday=(MO, TU, WE, TH, FR), dtstart=TODAY))
     pass
-
-get_hundred_weekdays()
