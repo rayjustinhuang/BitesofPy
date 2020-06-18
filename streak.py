178e02453d427c21b1c139b21cc0f438175618a2
Justin Huang <rayjustinhuang@gmail.com>
2020-06-18 20:48:13 +0800

    Add interim version of bite 139 (streak.py)

---
+++ streak.py
@@ -0,0 +1,49 @@
+from datetime import datetime, timedelta, date
+
+TODAY = date(2018, 11, 12)
+
+
+def extract_dates(data):
+    """Extract unique dates from DB table representation as shown in Bite"""
+    dates = []
+    
+    for line in data.splitlines():
+        if line[6:8] == "20":
+            dates.append(datetime.strptime(line[6:16], '%Y-%m-%d').date())
+            
+    return dates
+    pass
+
+
+def calculate_streak(dates):
+    """Receives sequence (set) of dates and returns number of days
+       on coding streak.
+
+       Note that a coding streak is defined as consecutive days coded
+       since yesterday, because today is not over yet, however if today
+       was coded, it counts too of course.
+
+       So as today is 12th of Nov, having dates 11th/10th/9th of Nov in
+       the table makes for a 3 days coding streak.
+
+       See the tests for more examples that will be used to pass your code.
+    """
+    pass
+
+data = """
+    +------------+------------+---------+
+    | date       | activity   | count   |
+    |------------+------------+---------|
+    | 2018-11-10 | pcc        | 1       |
+    | 2018-11-09 | 100d       | 1       |
+    | 2018-11-07 | 100d       | 2       |
+    | 2018-10-23 | pcc        | 1       |
+    | 2018-10-15 | pcc        | 1       |
+    | 2018-10-05 | bite       | 1       |
+    | 2018-09-21 | bite       | 4       |
+    | 2018-09-18 | bite       | 2       |
+    | 2018-09-18 | bite       | 4       |
+    +------------+------------+---------+
+    """
+
+extract_dates(data)
