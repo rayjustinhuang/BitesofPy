0a7f27caaf4afc15ca15c233fcfb20bb510042d0
Justin Huang <rayjustinhuang@gmail.com>
2020-07-15 22:42:39 +0800

    Add interim version of bite 217 (helplen.py)

--- helplen.py
+++ helplen.py
@@ -8,6 +8,9 @@ def get_len_help_text(builtin: BuiltinFunctionType) -> int:
        You need to redirect stdout from the help builtin.
        If the the object passed in is not a builtin, raise a ValueError.
     """
+    if type(builtin) != BuiltinFunctionType:
+        raise ValueError
+    
     f = StringIO()
     with redirect_stdout(f):
         print(builtin.help())
