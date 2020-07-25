e3dd68f37b7f869ee28bfc4b4d416d6f53a319e0
Justin Huang <rayjustinhuang@gmail.com>
2020-07-25 19:40:14 +0800

    Add belt.py (bite 72)

--- belt.py
+++ belt.py
@@ -4,18 +4,15 @@ scores = [10, 50, 100, 175, 250, 400, 600, 800, 1000]
 belts = 'white yellow orange green blue brown black paneled red'.split()
 HONORS = zip(scores, belts)
 
-
-def get_belt(user_score):
-    belt_dict = collections.OrderedDict()
+belt_dict = collections.OrderedDict(HONORS)
     
-    for scores, belt in HONORS:
-        belt_dict[scores] = belt
+def get_belt(user_score):
         
     if user_score < min(belt_dict.keys()):
         return None
         
-    for score, belt in belt_dict.items():
-        if user_score <= score:
+    for score, belt in reversed(list(belt_dict.items())):
+        if user_score >= score:
             return belt
         else:
             continue
