6931db733b70984bc7058386a6435a6ddaa0d149
Justin Huang <rayjustinhuang@gmail.com>
2020-08-01 15:39:06 +0800

    Add interim version of bite 171 (animation.py)

---
+++ animation.py
@@ -0,0 +1,24 @@
+from itertools import cycle
+import sys
+from time import time, sleep
+
+SPINNER_STATES = ['-', '\\', '|', '/']  # had to escape \
+STATE_TRANSITION_TIME = 0.1
+
+
+def spinner(seconds):
+    """Make a terminal loader/spinner animation using the imports aboveself.
+       Takes seconds argument = time for the spinner to runself.
+       Does not return anything, only prints to stdout."""
+    spinner_cycle = cycle(SPINNER_STATES)
+    
+    for _ in range(int(seconds/STATE_TRANSITION_TIME)):
+        sys.stdout.write(next(spinner_cycle))
+        sys.stdout.flush()
+        sleep(STATE_TRANSITION_TIME)
+        sys.stdout.write('\b')
+    pass
+
+
+if __name__ == '__main__':
+    spinner(2)
