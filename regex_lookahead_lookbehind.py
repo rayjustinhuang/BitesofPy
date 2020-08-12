fc28cf4a185c8d7db10dfc0bbf8e7e62b5e5438d
Justin Huang <rayjustinhuang@gmail.com>
2020-08-13 00:02:24 +0800

    Add interim version of bite 280 (regex_lookahead_lookbehind.py)

--- regex_lookahead_lookbehind.py
+++ regex_lookahead_lookbehind.py
@@ -22,8 +22,10 @@ def count_n_reps_or_n_chars_following(text, n=1, char=""):
     n: How often character should be repeated, defaults to 1
     char: Character which also counts if repeated n times
     """
-    findall_n_reps = re.findall(rf'(.)\1{{n}}', text)
-
+    findall_n_reps = re.findall(rf'(.)(?={n})', text)
+    findall_n_chars_following = re.findall(rf'(.)(?={char})', text)
+    
+    return len(findall_n_reps) + len(findall_n_chars_following)
 
 def check_surrounding_chars(text, surrounding_chars):
     """
