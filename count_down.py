c67b3edfffa9175cac6932363d9e564ca2a71f48
Justin Huang <rayjustinhuang@gmail.com>
2020-08-28 19:49:28 +0800

    Add interim version of bite 76 (count_down.py)

---
+++ count_down.py
@@ -0,0 +1,12 @@
+from functools import singledispatch
+
+
+@singledispatch
+def count_down(data_type):
+    # TODO: Learn how to use singledispatch!
+    count = len(data_type)
+    
+    while count != 0:
+        print(data_type[:count])
+        count -= 1
+    pass
