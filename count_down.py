180eede91223670b25b37c98e0ce5e65dfd59fc8
Justin Huang <rayjustinhuang@gmail.com>
2020-09-02 22:19:05 +0800

    Add interim version of bite 76 (count_down.py)

--- count_down.py
+++ count_down.py
@@ -26,3 +26,12 @@ def _(arg: list):
 @count_down.register
 def _(arg: set):
     count_down(''.join(str(i) for i in arg))
+    
+@count_down.register
+def _(arg: dict):
+    count_down(''.join(str(i) for i in arg.keys()))
+
+@count_down.register
+def _(arg: range):
+    count_down(list(arg))
+
