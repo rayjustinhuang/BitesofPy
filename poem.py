e69234866d7a03e0c59461f58a379b75889e2c36
Justin Huang <rayjustinhuang@gmail.com>
2020-09-08 22:34:26 +0800

    Add poem.py (bite 54)

--- poem.py
+++ poem.py
@@ -5,33 +5,16 @@ def print_hanging_indents(poem):
     
     split_poem = poem.strip().splitlines()
     
-    for i in range(len(split_poem)):
-        if i == len(split_poem)-1:
-            print('    '+split_poem[i].strip())
+    length = len(split_poem)
+    
+    for i in range(length):
+        if i == 0:
+            print(split_poem[i].strip())
         elif split_poem[i] == "":
             continue
-        elif split_poem[i+1].strip() != "":
-            print(split_poem[i].strip())
-        else:
+        elif split_poem[i-1].strip() != "":
+            #print(split_poem[i+1].strip())
             print("    "+split_poem[i].strip())
+        else:
+            print(split_poem[i].strip())
     pass
-
-shakespeare_unformatted = """
-                          To be, or not to be, that is the question:
-                          Whether 'tis nobler in the mind to suffer
-
-                          The slings and arrows of outrageous fortune,
-                          Or to take Arms against a Sea of troubles,
-                          """
-
-#print_hanging_indents(shakespeare_unformatted)
-
-"""
-        try:
-            if split_poem[i+1] != "":
-                print(split_poem[i].strip())
-            else:
-                print("    "+split_poem[i].strip())
-        except:
-            print('    '+split_poem[i].strip())
-"""
