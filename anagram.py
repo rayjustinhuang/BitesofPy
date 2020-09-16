e7cb913754c04eeb0f1d95bb647d9254062148a9
Justin Huang <rayjustinhuang@gmail.com>
2020-09-16 22:00:35 +0800

    Add interim version of bite 122 (anagram.py)

---
+++ anagram.py
@@ -0,0 +1,10 @@
+def is_anagram(word1, word2):
+    """Receives two words and returns True/False (boolean) if word2 is
+       an anagram of word1, ignore case and spacing.
+       About anagrams: https://en.wikipedia.org/wiki/Anagram"""
+       
+    word1_list = [i for i in word1.lower() if i != " "]
+    word2_list = [j for j in word2.lower() if j != " "]
+    
+    return word1_list.sort() == word2_list.sort()
+    pass
