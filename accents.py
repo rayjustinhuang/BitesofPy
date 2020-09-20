1b390badae93fa770255f92ce252da5d1410aeb4
Justin Huang <rayjustinhuang@gmail.com>
2020-09-20 22:31:32 +0800

    Add interim version of bite 157 (accents.py)

---
+++ accents.py
@@ -0,0 +1,14 @@
+import unicodedata
+
+def filter_accents(text):
+    """Return a sequence of accented characters found in
+       the passed in lowercased text string
+    """
+    accented_chars = set()
+    
+    for i in text:
+        if unicodedata.decomposition(i) != "":
+            accented_chars.add(i)
+            
+    return sorted(list(accented_chars))
+    pass
