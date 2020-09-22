3ff95a5fb488aa13e835d8214b8f40d016606a10
Justin Huang <rayjustinhuang@gmail.com>
2020-09-22 21:02:02 +0800

    Add interim version of bite 53 (text2cols.py)

---
+++ text2cols.py
@@ -0,0 +1,23 @@
+import textwrap
+
+COL_WIDTH = 20
+
+
+def text_to_columns(text):
+    """Split text (input arg) to columns, the amount of double
+       newlines (\n\n) in text determines the amount of columns.
+       Return a string with the column output like:
+       line1\nline2\nline3\n ... etc ...
+       See also the tests for more info."""
+    lines = [line.strip() for line in text.splitlines() if line.strip() != ""]
+        
+    wrapped_lines = [textwrap.wrap(line, COL_WIDTH) for line in lines]
+    
+    print(wrapped_lines)
+    pass
+
+text = """My house is small but cosy.
+
+    It has a white kitchen and an empty fridge."""
+
+text_to_columns(text)
