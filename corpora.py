a3994d6f68eef16f655494ca8b751b7fd8836d13
Justin Huang <rayjustinhuang@gmail.com>
2020-10-08 21:42:22 +0800

    Add interim version of bite 174 (corpora.py)

--- corpora.py
+++ corpora.py
@@ -34,6 +34,47 @@ Now we are engaged in a great civil war, testing whether that nation, or any nat
 But, in a larger sense, we cannot dedicate—we cannot consecrate—we cannot hallow—this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us—that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion—that we here highly resolve that these dead shall not have died in vain—that this nation, under God, shall have a new birth of freedom— and that government of the people, by the people, for the people, shall not perish from the earth."""
 EXTRA_CHAR = ["—", "\n", "  "]
 
+TAX_SYSTEM_IN_US = """Suppose that every day, ten men go out for beer, and the bill for all ten comes to $100.  If they paid their bill the way we pay our taxes (by taxpayer decile), it would go something like this:
+
+The first four men (the poorest) would pay nothing.
+The fifth would pay $1.
+The sixth would pay $3.
+The seventh would pay $7.
+The eighth would pay $12.
+The ninth would pay $18.
+The tenth man (the richest) would pay $59.
+
+So, that’s what they decided to do.
+
+The ten men drank in the bar every day and seemed quite happy with the arrangement, until one day, the owner threw them a curve ball.  “Since you’re all such good customers,” he said, “I’m going to reduce the cost of your daily beer by $20.”  Drinks for the ten men would now cost just $80.
+
+The group still wanted to pay their bill the way we pay our taxes.  So the first four men were unaffected. They would still drink for free.  But what about the other six?  How could they divide up the $20 windfall so that everyone would get his fair share?
+
+The bar owner suggested that it would be fair to reduce each man’s bill by a higher percentage the poorer he was, to follow the principle of the tax system they had been using, and he proceeded to suggest the new lower amounts each should now pay.
+
+And so the fifth man, like the first four, now paid nothing (a 100% saving).
+The sixth now paid $2 instead of $3 (a 33% saving).
+The seventh now paid $5 instead of $7 (a 29% saving).
+The eighth now paid $9 instead of $12 (a 25% saving).
+The ninth now paid $14 instead of $18 (a 22% saving).
+The tenth now paid $50 instead of $59 (a 15% saving).
+
+The first four continued to drink for free, and the latter six were all better off than before.  But, once outside the bar, the men began to compare their savings.
+
+“I only got a dollar out of the $20 saving,” declared the fifth man.  He pointed to the tenth man, “But he got $9!”
+
+“Yeah, that’s right,” exclaimed the sixth man.  “I only saved a dollar, too. It’s unfair that he saved nine times more than me!”
+
+“That’s true!” shouted the seventh man.  “Why should he get $9 back, when I got only $2?  The wealthy get all the breaks!”
+
+“Wait a minute,” yelled the first four men in unison, “we didn’t get anything at all.  This new tax system exploits the poor!”
+
+The nine men surrounded the tenth and beat him up.
+
+The next day, the tenth man didn’t show up, so the other nine sat down and had their beers without him.  But when it came time to pay the bill, they discovered something important: They didn’t have enough money between all of them for even half of the bill!
+
+And that is how our tax system works.  The people who already pay the highest taxes will naturally get the most benefit from a tax reduction.  Tax them too much, attack them for being wealthy, and they just may not show up anymore.  In fact, they might start drinking overseas, where the atmosphere is friendlier."""
+
 @dataclass
 class Corpora:
     """Add the inital variables along with creating any methods that
@@ -138,16 +179,15 @@ class Corpora:
         
         pass
 
-"""   
-test = Corpora(GETTYSBURG, stopwords = STOPWORDS)
+  
+#test = Corpora(TAX_SYSTEM_IN_US)
 
 #print(test.cleaned)
 
-test.extra = [EXTRA_CHAR[0]]
+#test.extra = [EXTRA_CHAR[0]]
 
 #print(test.extra)
 
 #print(test.cleaned)
 
-print(test.metrics)
-"""
+#print(test.metrics)
