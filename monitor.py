9a6e9061945230ad66dc6682884550a74412ecfc
Justin Huang <rayjustinhuang@gmail.com>
2020-10-17 21:29:15 +0800

    Add interim version of bite 88 (monitor.py)

---
+++ monitor.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from contextlib import contextmanager
+from datetime import date
+from time import time
+
+OPERATION_THRESHOLD_IN_SECONDS = 2.2
+ALERT_THRESHOLD = 3
+ALERT_MSG = 'ALERT: suffering performance hit today'
+
+violations = Counter()
+
+
+def get_today():
+    """Making it easier to test/mock"""
+    return date.today()
+
+
+@contextmanager
+def timeit():
+    start = get_today()
+    
+    yield
+    
+    end = get_today()
+    
+    violations += (end - start) % OPERATION_THRESHOLD_IN_SECONDS
+    
+    if violations.elements() >= ALERT_THRESHOLD:
+        print(ALERT_MSG)
+    pass
