358d3bc2038b0d82c2ecfd22ad62b5d0ec2d7e1b
Justin Huang <rayjustinhuang@gmail.com>
2020-11-27 00:27:14 +0800

    Add interim version of bite 284 (pascal.py)

--- pascal.py
+++ pascal.py
@@ -13,13 +13,16 @@ def pascal(N: int) -> List[int]:
         add_row[0] = 1
         add_row[-1] = 1
         
-        if i >= 3:
-            for j in range(1,i):
-                add_row[j] = triangle_rows[i-1][j-1] + triangle_rows[i-1][j]
+        if i == 3:
+            add_row[1] = triangle_rows[1][0] + triangle_rows[1][1]
+        
+        if i >= 4:
+            for j in range(1,i-1):
+                add_row[j] = triangle_rows[i-2][j-1] + triangle_rows[i-2][j]
                 
-        print(add_row)
+        #print(add_row)
         triangle_rows.append(add_row)
     # return row
-    print(triangle_rows)
+    return triangle_rows[N-1]
 
-pascal(5)
+#print(pascal(7))
