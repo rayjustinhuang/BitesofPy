6d462c9f3d82ea0d1bc5408e2b8a9d22198a6c86
Justin Huang <rayjustinhuang@gmail.com>
2020-12-17 22:46:53 +0800

    Add interim version of bite 241 (test_numbers_to_dec.py)

--- test_numbers_to_dec.py
+++ test_numbers_to_dec.py
@@ -2,10 +2,19 @@ import pytest
 
 from numbers_to_dec import list_to_decimal
 
-def check_nonnumber():
+def check_nonint():
     with pytest.raises(TypeError):
         list_to_decimal([1, 2, True])
+    with pytest.raises(TypeError):
+        list_to_decimal([1, 2, '5'])
+    with pytest.raises(TypeError):
+        list_to_decimal([1, 2.4, 8])
+    
         
-def check_negatives():
+def check_outofrange():
     with pytest.raises(ValueError):
         list_to_decimal([-3, 5, 9])
+        
+    with pytest.raises(ValueError):
+        list_to_decimal([11, 1, 2])
+        
