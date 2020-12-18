ef78d06d888c78957249fb7a2c3abcfa062288a6
Justin Huang <rayjustinhuang@gmail.com>
2020-12-18 20:15:53 +0800

    Add interim version of bite 241 (test_numbers_to_dec.py)

--- test_numbers_to_dec.py
+++ test_numbers_to_dec.py
@@ -14,7 +14,12 @@ def check_nonint():
 def check_outofrange():
     with pytest.raises(ValueError):
         list_to_decimal([-3, 5, 9])
-        
     with pytest.raises(ValueError):
         list_to_decimal([11, 1, 2])
+        
+def check_goodexamples():
+    assert list_to_decimal([4, 2, 6]) == 426
+    assert list_to_decimal([0, 5, 1, 6, 9, 8]) == 51698
+    assert list_to_decimal([3, 7]) == 37
+    assert list_to_decimal([7]) == 7
         
