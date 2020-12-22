16a73c993a6d7eab020aacba74bf2521694b67c6
Justin Huang <rayjustinhuang@gmail.com>
2020-12-22 19:13:18 +0800

    Add test_workouts.py (bite 246)

--- test_workouts.py
+++ test_workouts.py
@@ -2,7 +2,7 @@ import pytest
 
 from workouts import print_workout_days
 
-@pytest.mark.parametrize("test_input,expected", [("upper body #3", "No matching workout\n"), ("upper body #1", 'Mon\n'), ("30 min cardio", 'Wed\n')])
+@pytest.mark.parametrize("test_input,expected", [("upper body #4", "No matching workout\n"), ("upper body #1", 'Mon\n'), ("30 min cardio", 'Wed\n'), ('lower body #1, 30 min cardio', 'No matching workout\n'), ("upper body #2", 'Thu\n'),("lower body #2", 'Fri\n'),("lower body #1", 'Tue\n'),("", 'Mon, Tue, Wed, Thu, Fri\n'),])
 def test_print_workout_days(test_input, expected, capsys):
     print_workout_days(test_input)
     captured = capsys.readouterr()
