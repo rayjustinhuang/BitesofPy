ee0954f78ea373cacc5ca92072d6bc88d5120a5e
Justin Huang <rayjustinhuang@gmail.com>
2021-01-09 23:16:58 +0800

    Add interim version of constructors.py (bite 313)

---
+++ constructors.py
@@ -0,0 +1,21 @@
+import re
+
+
+class DomainException(Exception):
+    """Raised when an invalid is created."""
+
+
+class Domain:
+
+    def __init__(self, name):
+        # validate a current domain (r'.*\.[a-z]{2,3}$' is fine)
+        try:
+            re.match(r'.*\.[a-z]{2,3}$', name)
+        except:
+            raise DomainException
+        # if not valid, raise a DomainException
+        self.name = name
+        
+    # next add a __str__ method and write 2 class methods
+    # called parse_from_url and parse_from_email to construct domains
+    # from an URL and email respectively
