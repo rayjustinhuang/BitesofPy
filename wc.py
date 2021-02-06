0c48cc84ccb8c97e1e6df2cc419bee4fe5e4f236
Justin Huang <rayjustinhuang@gmail.com>
2021-02-06 19:22:19 +0800

    Add interim version of bite 96 (wc.py)

---
+++ wc.py
@@ -0,0 +1,22 @@
+def wc(file_):
+    """Takes an absolute file path/name, calculates the number of
+       lines/words/chars, and returns a string of these numbers + file, e.g.:
+       3 12 60 /tmp/somefile
+       (both tabs and spaces are allowed as separator)"""
+    with open(file_) as f:
+        text = f.read()
+        filename = f.name
+        lines = len(text.splitlines())
+        words = len(text.split(' '))
+        chars = len(text)
+        
+    #print(lines, words, chars)
+        
+    return f'{lines}\t{words}\t{chars}\t{filename}'
+    pass
+
+
+if __name__ == '__main__':
+    # make it work from cli like original unix wc
+    import sys
+    print(wc(sys.argv[1]))
