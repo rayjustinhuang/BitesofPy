a667bd450ed39f69a39a341181e40907c23479a4
Justin Huang <rayjustinhuang@gmail.com>
2021-02-20 19:23:47 +0800

    Add chessboard.py (bite 176)

--- chessboard.py
+++ chessboard.py
@@ -4,7 +4,8 @@ WHITE, BLACK = ' ', '#'
 def create_chessboard(size=8):
     """Create a chessboard with of the size passed in.
        Don't return anything, print the output to stdout"""
-    for i in size:
-        sequence = [WHITE, BLACK]*size/2
-        print(sequence)
+    for i in range(int(size/2)):
+        sequence = [WHITE, BLACK]*int(size/2)
+        print("".join(sequence))
+        print("".join(sequence[::-1]))
     pass
