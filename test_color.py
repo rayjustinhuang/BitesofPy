0d54ac5df8b5d2e6dac7983ce0402f58e9832fdb
Justin Huang <rayjustinhuang@gmail.com>
2021-02-25 22:05:28 +0800

    Add interim version of bite 247 (test_color.py)

--- test_color.py
+++ test_color.py
@@ -9,11 +9,12 @@ import color
 def gen():
     return color.gen_hex_color()
 
-test_colors = [(117, 156, 138, '#759C8A')]
+test_colors = [(117, 156, 138, '#759C8A'),
+                (25, 100, 50, '#196432')]
 
 @pytest.mark.parametrize('red, blue, green, expected', test_colors)
 @patch('color.sample')
-def test_gen_hex_color(gen, red, blue, green, expected, mock_sample):
+def test_gen_hex_color(mock_sample, gen, red, blue, green, expected):
     mock_sample.return_value = red, blue, green
     assert next(gen) == expected
     pass
