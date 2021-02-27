7cc7a139b6e33292eca646c2c527ff66798aeb12
Justin Huang <rayjustinhuang@gmail.com>
2021-02-27 19:03:39 +0800

    Add interim version of bite 247 (test_color.py)

--- test_color.py
+++ test_color.py
@@ -14,20 +14,22 @@ test_colors = [(117, 156, 138, '#759C8A'),
                 (203, 196, 204, '#CBC4CC'),
                 (1, 1, 1, '#010101'),
                 (255, 255, 255, '#FFFFFF')]
+                
+def mock_sample(sample_space, n):
+    if sample_space != range(0, 256):
+        raise ValueError
+    if n != 3:
+        raise ValueError
+    return (117,156,138)
 
 
-@pytest.mark.parametrize('red, blue, green, expected', test_colors)
-def test_gen_hex_color(gen, red, blue, green, expected):
-    with patch('color.sample') as mock_sample:
+#@pytest.mark.parametrize('red, blue, green, expected', test_colors)
+#@patch('color.sample', mock_sample)
+def test_gen_hex_color(mock_sample, gen):
+    with patch('color.sample', mock_sample) as mock_sample:
         mock_sample.assert_called_with(range(0, 256), 3)
         mock_sample.return_value = red, blue, green
-        assert next(gen) == expected
+        assert next(gen) == '#759C8A'
     pass
 
                 
-#def mock_sample(sample_space, n):
-#    if sample_space != range(0, 256):
-#        raise ValueError
-#    if n != 3:
-#        raise ValueError
-#    return sample(range(0, 256), 3)
