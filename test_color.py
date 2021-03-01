6f08900043b6ad4934c454fc8f606f4b3c6d7da2
Justin Huang <rayjustinhuang@gmail.com>
2021-03-01 22:39:44 +0800

    Add test_color.py (bite 247)

--- test_color.py
+++ test_color.py
@@ -15,20 +15,23 @@ test_colors = [(117, 156, 138, '#759C8A'),
                 (1, 1, 1, '#010101'),
                 (255, 255, 255, '#FFFFFF')]
                 
-def mock_sample(sample_space, n):
+def replace_sample(sample_space, n):
     if sample_space != range(0, 256):
         raise ValueError
     if n != 3:
         raise ValueError
     return (117,156,138)
 
-
-#@pytest.mark.parametrize('red, blue, green, expected', test_colors)
-@patch('color.sample', mock_sample)
-def test_gen_hex_color(mock_sample, gen):
-    mock_sample.return_value = red, blue, green
+@patch('color.sample', replace_sample)
+def test_gen_hex_color(replace_sample, gen):
     assert next(gen) == '#759C8A'
-    mock_sample.assert_called_with(range(0, 256), 3)
+    replace_sample.assert_called_with(range(0, 256), 3)
     pass
 
-                
+@pytest.mark.parametrize('red, blue, green, expected', test_colors)
+@patch('color.sample')
+def test_gen_hex_color(mock_sample, gen, red, blue, green, expected):
+    mock_sample.return_value = red, blue, green
+    assert next(gen) == expected
+    mock_sample.assert_called_with(range(0, 256), 3)
+    pass
