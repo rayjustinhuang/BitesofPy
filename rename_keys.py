2619f33cbd296fdc9e425b11aec818cfefc37eb8
Justin Huang <rayjustinhuang@gmail.com>
2021-05-06 23:02:03 +0800

    Add interim version of bite 297

--- rename_keys.py
+++ rename_keys.py
@@ -38,11 +38,24 @@ def iterdict(input_dict):
 def rename_keys(data: Dict[Any, Any]) -> Dict[Any, Any]:
     change = data.copy()
     
-    change = strip_at_signs(change)
+    new_dict = {}
     
-    iterdict(change)
-            
-    return change
+    input_dict = strip_at_signs(change)
+    
+    for key, val in input_dict.items():
+        if type(val) == dict:
+            val = strip_at_signs(val)
+            new_key = key.replace('@', '')
+            new_dict[key] = val
+        elif isinstance(val, Iterable):
+            for obj in val:
+                if type(obj) == dict:
+                    value = strip_at_signs(obj)
+            new_dict[key] = value
+        else:
+            new_dict[key] = val
+    
+    return new_dict
     
 test = {'@pii': {'@address': [{'@city': 'London'}, {'city': 'Moscow'}], '@email': 'jane@example.com', '@id': 12345, 'name': {'@first_name': 'Jane', '@last_name': 'Doe'}}}
 
