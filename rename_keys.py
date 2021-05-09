96a72fab36f943721e1c9caea5ca8151c0e3c610
Justin Huang <rayjustinhuang@gmail.com>
2021-05-09 18:39:29 +0800

    Add interim version of bite 297

--- rename_keys.py
+++ rename_keys.py
@@ -15,25 +15,13 @@ def strip_at_signs(input_dict):
         
     return new_dict
     
-def iterdict(input_dict):
-    new_dict = {}
+def dict_in_list(input_list):
+    new_list = []
     
-    input_dict = strip_at_signs(input_dict)
-    
-    for key, val in input_dict.items():
-        if type(val) == dict:
-            val = strip_at_signs(val)
-            new_key = key.replace('@', '')
-            new_dict[key] = val
-            iterdict(val)
-        elif isinstance(val, Iterable):
-            for obj in val:
-                if type(obj) == dict:
-                    iterdict(obj)
-        else:
-            new_dict[key] = val
-    
-    return new_dict
+    for item in input_list:
+        new_list.append(strip_at_signs(item))
+        
+    return new_list
 
 def rename_keys(data: Dict[Any, Any]) -> Dict[Any, Any]:
     change = data.copy()
@@ -46,13 +34,13 @@ def rename_keys(data: Dict[Any, Any]) -> Dict[Any, Any]:
         if type(val) == dict:
             val = strip_at_signs(val)
             new_key = key.replace('@', '')
-            new_dict[key] = val
+            new_dict[new_key] = val
+            rename_keys(val)
         elif isinstance(val, list):
-            new_val = []
-            for obj in val:
-                if type(obj) == dict:
-                    new_val.append(strip_at_signs(obj))
-            new_dict[key] = new_val
+            print(val)
+            new_key = key.replace('@', '')
+            new_val = dict_in_list(val)
+            new_dict[new_key] = new_val
         else:
             new_dict[key] = val
     
@@ -60,6 +48,5 @@ def rename_keys(data: Dict[Any, Any]) -> Dict[Any, Any]:
     
 test = {'@pii': {'@address': [{'@city': 'London'}, {'city': 'Moscow'}], '@email': 'jane@example.com', '@id': 12345, 'name': {'@first_name': 'Jane', '@last_name': 'Doe'}}}
 test2 = {'@user_name': 'jdoe', 1: 'one', 2: 'two', '@three': 3}
-# print(iterdict(test))
 
-print(rename_keys(test2))
+print(rename_keys(test))
