e2245708754087e1572dcc70904a575e491f3356
Justin Huang <rayjustinhuang@gmail.com>
2021-05-11 22:02:51 +0800

    Add interim version of bite 297 (rename_keys)

--- rename_keys.py
+++ rename_keys.py
@@ -1,5 +1,6 @@
 from typing import Dict, Any
 from collections.abc import Iterable
+import copy
 
 def strip_at_signs(input_dict):
     new_keys = []
@@ -24,7 +25,7 @@ def dict_in_list(input_list):
     return new_list
 
 def rename_keys(data: Dict[Any, Any]) -> Dict[Any, Any]:
-    change = data.copy()
+    change = copy.deepcopy(data)
     
     new_dict = {}
     
@@ -32,21 +33,61 @@ def rename_keys(data: Dict[Any, Any]) -> Dict[Any, Any]:
     
     for key, val in input_dict.items():
         if type(val) == dict:
-            val = strip_at_signs(val)
             new_key = key.replace('@', '')
-            new_dict[new_key] = val
-            rename_keys(val)
+            sub_dict = {}
+            
+            for key, value in val.items():
+
+                if isinstance(value, list):
+                    new_val = []
+                    for item in value:
+                        if type(item) == list:
+                            new_val.append(dict_in_list(item))
+                        elif type(item) == dict:
+                            new_val.append(strip_at_signs(item))
+                        else:
+                            new_val.append(item)
+                    print(new_val)
+                    sub_dict[new_key] = new_val
+                else:
+                    sub_dict[new_key] = value
+                    
+            sub_dict = strip_at_signs(sub_dict)
+            
+            new_dict[new_key] = sub_dict
+                    
         elif isinstance(val, list):
-            print(val)
             new_key = key.replace('@', '')
             new_val = dict_in_list(val)
             new_dict[new_key] = new_val
         else:
             new_dict[key] = val
-    
+
     return new_dict
     
 test = {'@pii': {'@address': [{'@city': 'London'}, {'city': 'Moscow'}], '@email': 'jane@example.com', '@id': 12345, 'name': {'@first_name': 'Jane', '@last_name': 'Doe'}}}
 test2 = {'@user_name': 'jdoe', 1: 'one', 2: 'two', '@three': 3}
+# print(iterdict(test))
 
 print(rename_keys(test))
+
+    
+def iterdict(input_dict):
+    new_dict = {}
+    
+    input_dict = strip_at_signs(input_dict)
+    
+    for key, val in input_dict.items():
+        if type(val) == dict:
+            val = strip_at_signs(val)
+            new_key = key.replace('@', '')
+            new_dict[key] = val
+            iterdict(val)
+        elif isinstance(val, Iterable):
+            for obj in val:
+                if type(obj) == dict:
+                    iterdict(obj)
+        else:
+            new_dict[key] = val
+    
+    return new_dict
