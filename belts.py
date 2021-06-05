ae7c46e54aa37eee1515a69321c4e77ae93fee31
Justin Huang <rayjustinhuang@gmail.com>
2021-06-05 21:43:44 +0800

    Add interim version of bite 237 (belts.py)

---
+++ belts.py
@@ -0,0 +1,40 @@
+import json
+from pathlib import Path
+from urllib.request import urlretrieve
+
+SCORES = [10, 50, 100, 175, 250, 400, 600, 800, 1000]
+BELTS = ('white yellow orange green blue brown black '
+         'paneled red').split()
+TMP = Path('/tmp')
+
+def get_data(file_no=1, tmp=TMP):
+    file_name = f'bite_scores{file_no}.json'
+    file_path = TMP / file_name
+    remote = 'https://bites-data.s3.us-east-2.amazonaws.com/'
+    if not file_path.exists():
+        urlretrieve(f'{remote}{file_name}',
+                    file_path)
+    return file_path
+
+def get_belts(data: str) -> dict:
+    """Parsed the passed in json data:
+       {"date":"5/1/2019","score":1},
+       {"date":"9/13/2018","score":3},
+       {"date":"10/25/2019","score":1},
+
+       Loop through the scores in chronological order,
+       determining when belts were achieved (use SCORES
+       and BELTS).
+
+       Return a dict with keys = belts, and values =
+       readable dates, example entry:
+       'yellow': 'January 25, 2018'
+    """
+    #print(data)
+    with open(data) as f:
+        json_data = json.load(f)
+    
+    print(json_data)
+    pass
+
+get_belts(data)
