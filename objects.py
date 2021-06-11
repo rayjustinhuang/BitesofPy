0f8ca3fcc5d45213d84ee475f36f4d6c1429419e
Justin Huang <rayjustinhuang@gmail.com>
2021-06-11 23:33:45 +0800

    Add interim version of bite 312 (objects.py)

---
+++ objects.py
@@ -0,0 +1,25 @@
+from typing import Dict, List
+import types
+import keyword
+
+scores = {
+    "builtin": 1,
+    "keyword": 2,
+    "module": 3,
+}
+
+
+def score_objects(objects: List[str],
+                  scores: Dict[str, int] = scores) -> int:
+                      
+    running_total = 0
+    for item in objects:
+        if isinstance(item, types.BuiltinFunctionType):
+            running_total += scores['builtin']
+        elif keyword.iskeyword(item):
+            running_total += scores['keyword']
+        else:
+            running_total += scores['module']
+            
+    return running_total
+    pass
