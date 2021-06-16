b21af63c3cc1ee083580bccd40b64429b7294250
Justin Huang <rayjustinhuang@gmail.com>
2021-06-16 23:25:35 +0800

    Add interim version of bite 312 (objects.py)

--- objects.py
+++ objects.py
@@ -4,6 +4,7 @@ import keyword
 import inspect
 import builtins
 import sys
+import pkg_resources
 
 scores = {
     "builtin": 1,
@@ -15,14 +16,17 @@ scores = {
 def score_objects(objects: List[str],
                   scores: Dict[str, int] = scores) -> int:
                       
+    installed_packages = pkg_resources.working_set
+    installed_packages_list = ["%s" % (i.key) for i in installed_packages]
+                      
     running_total = 0
     for item in objects:
-        if item in dir(builtins):
+        if item in installed_packages_list:
+            running_total += scores['module']
+        elif item in dir(builtins):
             running_total += scores['builtin']
         elif keyword.iskeyword(item):
             running_total += scores['keyword']
-        elif item in sys.builtin_module_names:
-            running_total += scores['module']
         else:
             continue
             
@@ -32,6 +36,8 @@ def score_objects(objects: List[str],
 test = ['raise', 'random']
 test2 = ['any', 'all', 'max']
 
-print(score_objects(test, scores))
+#print(score_objects(test, scores))
 
-print(isinstance('random', types.ModuleType))
+#print(isinstance('random', types.ModuleType))
+
+#help('modules')
