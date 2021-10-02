fa192cf10dd1fe7d9de68efded63e1869f831888
Justin Huang <rayjustinhuang@gmail.com>
2021-10-02 20:23:39 +0800

    Add interim version of bite 36 (profile. py)

--- profile.py
+++ profile.py
@@ -3,6 +3,16 @@ def get_profile(name, age: int, *sports, **awards):
         raise ValueError
     if len(sports) > 5:
         raise ValueError
+        
+    if sports and not awards:
+        sports = list(sports)
+        return {'name': name, 'age': age, 'sports': sports}
     
+    if not sports and awards:
+        return {'name': name, 'age': age, 'awards': awards}
+        
+    if not sports and not awards:
+        return {'name': name, 'age': age}
+        
     return {'name': name, 'age': age, 'sports': sports, 'awards': awards}
     pass
