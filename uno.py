03b694c5259ff607621419d9677c5caff90a6057
Justin Huang <rayjustinhuang@gmail.com>
2021-10-19 20:31:50 +0800

    Add interim version of bite 60 (uno.py)

---
+++ uno.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+
+SUITS = 'Red Green Yellow Blue'.split()
+
+UnoCard = namedtuple('UnoCard', 'suit name')
+
+
+def create_uno_deck():
+    """Create a deck of 108 Uno cards.
+       Return a list of UnoCard namedtuples
+       (for cards w/o suit use None in the namedtuple)"""
+    
+    deck = []
+    
+    for suit in SUITS:
+        zeroes = UnoCard(suit, 0)
+        numbers = [2*UnoCard(suit, x) for x in range(1, 10)]
+        draw_two = 2*UnoCard(suit, 'Draw Two')
+        skip = 2*UnoCard(suit, 'Skip')
+        reverse = 2*UnoCard(suit, 'Reverse')
+        deck.append(zeroes)
+        deck += numbers
+        deck.append(draw_two)
+        deck.append(skip)
+        deck.append(reverse)
+        
+    deck += [4*UnoCard(None, 'Wild'), 4*UnoCard(None, 'Wild Draw')]
+    
+    return deck
+    pass
