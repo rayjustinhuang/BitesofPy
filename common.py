e0433972a8ce92be3c7dc499e66bc59b36d497b0
Justin Huang <rayjustinhuang@gmail.com>
2021-11-07 00:48:21 +0800

    Add interim version of bite 78 (common.py)

---
+++ common.py
@@ -0,0 +1,11 @@
+def common_languages(programmers):
+    """Receive a dict of keys -> names and values -> a sequence of
+       of programming languages, return the common languages"""
+       
+    names = programmers.keys()
+    
+    common = []
+    
+    for name in names:
+        programmers[name]
+    pass
