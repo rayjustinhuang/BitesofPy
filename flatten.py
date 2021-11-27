2b4b79865986ae1253636c621b68dbaa672d0e7d
rayjustinhuang <rayjustinhuang@gmail.com>
2021-11-27 22:52:15 +0800

    Add flatten.py (bite 84)

--- flatten.py
+++ flatten.py
@@ -1,9 +1,9 @@
 def flatten(list_of_lists):
     if list_of_lists == []:
-        return list_of_lists
-    elif type(list_of_lists[0]) == list:
-        return flatten(list_of_lists[0]) + flatten(list_of_lists[1:])
+        return list(list_of_lists)
+    elif type(list_of_lists[0]) == list or type(list_of_lists[0]) == tuple:
+        return flatten(list(list_of_lists[0])) + flatten(list(list_of_lists[1:]))
 
-    return list_of_lists[:1] + flatten(list_of_lists[1:]) 
+    return list(list_of_lists[:1]) + flatten(list(list_of_lists[1:])) 
     
     pass
