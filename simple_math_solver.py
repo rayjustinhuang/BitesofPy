e98220c0f1a3701acce598f708e26624a6b48bd1
Justin Huang <rayjustinhuang@gmail.com>
2021-11-29 20:16:51 +0800

    Add interim version of bite 330 (simple_math_solver.py)

--- simple_math_solver.py
+++ simple_math_solver.py
@@ -7,10 +7,19 @@ def find_all_solutions(
     operator_path: List[str], expected_result: int
 ) -> Union[List[List[int]], Iterable[List[int]]]:
     # TODO: blank canvas to fill
+    if type(expected_result) != int:
+        raise ValueError
     pass
 
-def addition(expected_result: int):
-    possible = permutations(range(1,expected_result), 2)
-    return [x for x in possible if sum(x) == expected_result]
+def operation(op: str, expected_result: int):
+    if op == '+':
+        possible = permutations(range(1,expected_result), 2)
+        return [x for x in possible if sum(x) == expected_result]
+    possible = permutations(range(1,10),2)
+    if op == '-':
+        return [x for x in possible if (x[0] - x[1]) == expected_result]
+    if op == '*':
+        return [x for x in possible if (x[0] * x[1]) == expected_result]
     
-print(addition(6))
+    
+print(operation('-', 5))
