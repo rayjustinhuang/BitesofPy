a3cacb491e6c9b6a7fc7894b70e3fdb211fd693b
Justin Huang <rayjustinhuang@gmail.com>
2021-12-04 18:23:20 +0800

    Add interim version of bite 330 (simple_math_solver.py)

--- simple_math_solver.py
+++ simple_math_solver.py
@@ -14,13 +14,16 @@ def find_all_solutions(
             raise ValueError
             
     if len(operator_path) == 1:
-        return operation(operator_path, expected_result)
+        return basic_operation(operator_path, expected_result)
     else:
         possible = permutations(range(1, 9), len(operator_path)+1)
         
+
+            
+        
     pass
 
-def operation(op: str, expected_result: int):
+def basic_operation(op: str, expected_result: int):
     if op == '+':
         possible = permutations(range(1,expected_result), 2)
         return [x for x in possible if sum(x) == expected_result]
@@ -29,6 +32,26 @@ def operation(op: str, expected_result: int):
         return [x for x in possible if (x[0] - x[1]) == expected_result]
     if op == '*':
         return [x for x in possible if (x[0] * x[1]) == expected_result]
+        
+def long_operation(op: list, expected_result: int):
+    possible = permutations(range(1, 9), len(op)+1)
+    
+    for answer in possible:
+        working_answer = list(answer)
+        for operator in op:
+        
+        # Multiplication first
+        if operator == '*':
+            mul_index = op.index('*')
+            element = mul(working_answer[mul_index], working_answer[mul_index+1])
+            working_answer.pop(mul_index)
+            working_answer.pop(mul_index)
+            working_answer.insert(mul_index, element)
+            op.pop(mul_index)
+            
+        for operator in op:
+            
+            
     
     
 print(operation('-', 5))
