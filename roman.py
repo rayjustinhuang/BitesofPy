910299d59148739f525daec28b28b089463b48dc
Justin Huang <rayjustinhuang@gmail.com>
2022-01-11 13:28:49 -0800

    Add interim version of bite 87 (roman.py)

---
+++ roman.py
@@ -0,0 +1,6 @@
+def romanize(decimal_number):
+    """Takes a decimal number int and converts its Roman Numeral str"""
+    if decimal_number <= 0 or decimal_number >= 4000:
+        raise ValueError
+    
+    pass
