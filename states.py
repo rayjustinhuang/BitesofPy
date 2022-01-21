d6ee99ca46c8d402faee3502d8b2328bfc1d4801
Justin Huang <rayjustinhuang@gmail.com>
2022-01-21 15:27:10 -0800

    Add interim version of bite 89 (states.py)

--- states.py
+++ states.py
@@ -49,6 +49,11 @@ def get_state_abbrev(state_name, us_state_abbrev=us_state_abbrev):
        'Illinois' returns 'IL'.
        If the state is not in the dict, return 'N/A' which we stored
        in the NOT_FOUND constant (takeaway: dicts are great for lookups)"""
+       
+    try:
+        return us_state_abbrev[state_name]
+    else:
+        return NOT_FOUND
     pass
 
 
