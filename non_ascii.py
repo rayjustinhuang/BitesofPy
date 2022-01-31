97975e99954c6ae63f6d6074275d9e940bc82e60
Justin Huang <rayjustinhuang@gmail.com>
2022-01-31 21:01:02 +0800

    Add non_ascii.py (bite 113)

--- non_ascii.py
+++ non_ascii.py
@@ -1,11 +1,11 @@
 def extract_non_ascii_words(text):
     """Filter a text returning a list of non-ascii words"""
     word_list = text.split()
-    ascii_chars = [chr(i) for in in range(128)]
+    ascii_chars = set(chr(i) for i in range(128))
     result = []
     
     for word in word_list:
-        if word.intersection(ascii_chars) != set(list(word)):
+        if set(word).intersection(ascii_chars) != set(list(word)):
             result.append(word)
             
     return result
