216f5fd92a68cef325ac53787812de4aa92b0672
rayjustinhuang <rayjustinhuang@gmail.com>
2022-02-20 22:46:30 +0700

    Add pretty_string.py (bite 324)

--- pretty_string.py
+++ pretty_string.py
@@ -4,6 +4,6 @@ from typing import Any
 
 def pretty_string(obj: Any) -> str:
     # TODO: your code
-    result = pprint.pp(obj, depth = 2, width = 60)
+    result = pprint.pformat(obj, depth = 2, width = 60)
     return result
     pass
